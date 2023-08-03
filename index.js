3a4b6bc80654efd8801dfc28781e6e387e09eaab
gabrielsimplicio00 <gaasimplicio@gmail.com>
2023-08-02 23:28:50 -0300

    automate bot

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
 import puppeteer from "puppeteer";
 import nodemailer from "nodemailer";
+import cron from "node-cron";
 import dados from "./credentials.json" assert { type: "json" };
 import "dotenv/config";
 
-(async () => {
-  const browser = await puppeteer.launch({
-    // headless: false,
-    // slowMo: 100,
-  });
-  const page = await browser.newPage();
+cron.schedule("* * * * *", () => main()); // 0 10 1 * *
 
-  await page.setExtraHTTPHeaders({ "Cache-Control": "no-cache" });
+async function main() {
+  try {
+    const browser = await puppeteer.launch({
+      // headless: false,
+      // slowMo: 100,
+    });
+    const page = await browser.newPage();
 
-  // Navigate the page to a URL
-  await page.goto("https://meutim.tim.com.br/novo/login");
-  await page.waitForNavigation();
+    await page.setExtraHTTPHeaders({ "Cache-Control": "no-cache" });
 
-  page.setDefaultTimeout(180_000);
+    // Navigate the page to a URL
+    await page.goto("https://meutim.tim.com.br/novo/login");
+    await page.waitForNavigation();
 
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
+    page.setDefaultTimeout(180_000);
 
-  // Wait and click on first result
-  const inputNumero = await page.$("#campo-numero");
-  const inputSenha = await page.$("#campo-senha");
-  const btnEntrar = "#btn-entrar";
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
 
-  await inputNumero.type(dados.numero, { delay: 50 });
-  await inputSenha.type(dados.senha, { delay: 50 });
-  await page.click(btnEntrar);
+    // Wait and click on first result
+    const inputNumero = await page.$("#campo-numero");
+    const inputSenha = await page.$("#campo-senha");
+    const btnEntrar = "#btn-entrar";
 
-  await extractPDF(page);
+    await inputNumero.type(dados.numero, { delay: 50 });
+    await inputSenha.type(dados.senha, { delay: 50 });
+    await page.click(btnEntrar);
 
-  await browser.close();
-})();
+    await getFatura(page);
 
-async function extractPDF(page) {
-  await page.waitForTimeout(10_000);
+    await browser.close();
+  } catch (error) {
+    console.error("Erro ao executar o bot: ", error);
+    console.log("Reiniciando o bot...");
+    setTimeout(() => main(), 5000);
+  }
+}
+
+async function getFatura(page) {
+  page.setDefaultNavigationTimeout(60_000);
+  await page.waitForTimeout(15_000);
+
   const btnPagarFatura = "button[title='Pagar agora']";
-
-  // ".status-em-aberto .view-pdf";   button[title='Pagar agora'] button.btn-grito.btn-small.btn-verde
 
   await page.click(btnPagarFatura);
 
-  await sendInfo(page);
+  await sendMailWithContent(page);
 }
 
-async function sendInfo(page) {
-  await page.waitForTimeout(10_000);
+async function sendMailWithContent(page) {
+  await page.setDefaultNavigationTimeout(60_000);
+  await page.waitForTimeout(15_000);
 
   const codigoDeBarras = await page.$eval(
     ".code-bar",
@@ -66,9 +76,9 @@ async function sendInfo(page) {
 
   const message = {
     from: `Gabriel Simplicio <${process.env.EMAIL1}>`,
-    to: process.env.EMAIL2,
+    to: process.env.EMAIL1,
     subject: "Código de barras extrato Meu TIM",
-    text: `Código de barras da conta desse mês:
+    text: `Código de barras do extrato desse mês:
 
     ${codigoDeBarras}`,
   };
@@ -77,7 +87,7 @@ async function sendInfo(page) {
     if (err) {
       console.log("Erro ao enviar o e-mail:", err);
     } else {
-      console.log("E-mail enviado:", info.response);
+      console.log("E-mail enviado com sucesso! ", info.response);
     }
   });
 }
