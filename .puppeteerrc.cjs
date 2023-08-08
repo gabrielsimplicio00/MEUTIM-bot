027fc48efe3548b4c6bb3dc1543660dd707ba676
gabrielsimplicio00 <gaasimplicio@gmail.com>
2023-08-07 21:31:37 -0300

    teste

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,5 +1,5 @@
 const { join } = require("path");
 
 module.exports = {
-  cacheDirectory: join("/opt/render/project/src", ".cache", "puppeteer"),
+  cacheDirectory: "/opt/render/project/src/.cache/puppeteer",
 };
